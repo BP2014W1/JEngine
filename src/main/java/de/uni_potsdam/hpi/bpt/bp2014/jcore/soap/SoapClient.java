a401e5255b0531494c693ad1528e8ca707e23ad7
jaSunny <github@sunny.remote.mx>
2015-04-28 23:38:50 +0200

    forgot the actual package :D

---
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/soap/SoapClient.java
@@ -0,0 +1,68 @@
+package de.uni_potsdam.hpi.bpt.bp2014.jcore.soap;
+
+import de.uni_potsdam.hpi.bpt.bp2014.settings.Settings;
+
+import javax.xml.soap.*;
+
+public class SoapClient {
+
+
+    public Boolean SOAPinitializer() throws Exception {
+        // Create SOAP Connection
+        SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+        SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+
+        // Send SOAP Message to SOAP Server
+        SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(), Settings.EventSubscriber);
+
+        // print SOAP Response
+        System.out.print("Response SOAP Message:");
+        soapResponse.writeTo(System.out);
+
+        soapConnection.close();
+        return true;
+    }
+
+    public SOAPMessage createSOAPRequest() throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration("example", Settings.EventSubscriber);
+
+        /*
+        Constructed SOAP Request Message:
+        <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:example="http://ws.cdyne.com/">
+            <SOAP-ENV:Header/>
+            <SOAP-ENV:Body>
+                <example:VerifyEmail>
+                    <example:email>mutantninja@gmail.com</example:email>
+                    <example:LicenseKey>123</example:LicenseKey>
+                </example:VerifyEmail>
+            </SOAP-ENV:Body>
+        </SOAP-ENV:Envelope>
+         */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("VerifyEmail", "example");
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("email", "example");
+        soapBodyElem1.addTextNode("mutantninja@gmail.com");
+        SOAPElement soapBodyElem2 = soapBodyElem.addChildElement("LicenseKey", "example");
+        soapBodyElem2.addTextNode("123");
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", Settings.EventSubscriber  + "VerifyEmail");
+
+        soapMessage.saveChanges();
+
+        /* Print the request message */
+        System.out.print("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println();
+
+        return soapMessage;
+    }
+}
